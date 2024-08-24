0a48f45f7de5988433f8855543e94e5ccf88ae6d
AKSAR <akshatjaiswal2518@gmail.com>
2024-08-25 00:09:38 +0530

    add post and comment model

---
+++ server/src/models/comment.model.js
@@ -0,0 +1,21 @@
+import mongoose, { Schema } from "mongoose";
+
+const commentSchema = new Schema(
+    {
+        post: {
+            type: Schema.Types.ObjectId,
+            ref: "Post",
+        },
+        owner: {
+            type: Schema.Types.ObjectId,
+            ref: "User",
+        },
+        content: {
+            type: String,
+            required: true,
+        },
+    },
+    { timestamps: true }
+);
+
+export const Comment = mongoose.model("Comment", commentSchema);
