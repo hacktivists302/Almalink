9c7375a948acb97a66e0d2a0fd0772d2401c4527
AKSAR <akshatjaiswal2518@gmail.com>
2024-08-28 19:17:51 +0530

    changes in configuration

--- server/src/app.js
+++ server/src/app.js
@@ -2,12 +2,11 @@ import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 
-
 const app = express();
 
 app.use(
     cors({
-        origin: process.env.PORT,
+        origin: process.env.CORS_ORIGIN,
         credentials: true,
     })
 );
